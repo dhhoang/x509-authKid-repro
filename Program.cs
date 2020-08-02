92ced3b0cce94b127601b0463ac9bcd2245e208a
Huy Hoang <goaty1992@outlook.com>
2020-08-02 19:21:34 +0700

    1st

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace x509_authKid_repro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string authorityKeyIdentifierOid = "2.5.29.35";
+            var cert = new X509Certificate2("me.crt");
+            var ext = cert.Extensions[authorityKeyIdentifierOid];
+            Console.WriteLine(ext.Format(false));
+        }
+    }
+}
